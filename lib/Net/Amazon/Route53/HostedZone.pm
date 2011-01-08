6fff830a83b2376e45972e9557880591a021073c
Marco Fontani <mfontani@cpan.org>
2011-01-08 22:32:07 +0000

    Get nameservers for zone

--- lib/Net/Amazon/Route53/HostedZone.pm
+++ lib/Net/Amazon/Route53/HostedZone.pm
@@ -3,6 +3,7 @@ use warnings;
 
 package Net::Amazon::Route53::HostedZone;
 use Mouse;
+use XML::Bare;
 
 has 'route53' => ( is => 'rw', isa => 'Net::Amazon::Route53', required => 1, );
 
@@ -10,6 +11,20 @@ has 'id'              => ( is => 'rw', isa => 'Str', required => 1, default => '
 has 'name'            => ( is => 'rw', isa => 'Str', required => 1, default => '' );
 has 'callerreference' => ( is => 'rw', isa => 'Str', required => 1, default => '' );
 has 'comment'         => ( is => 'rw', isa => 'Str', required => 1, default => '' );
+
+has 'nameservers' => ( is => 'rw', isa => 'ArrayRef[Str]', required => 1, default => sub { [] } );
+
+sub BUILD
+{
+    my $self = shift;
+
+    my $rc = $self->route53->request('get','https://route53.amazonaws.com/2010-10-01/' . $self->id);
+    my $resp = XML::Bare::xmlin($rc->decoded_content);
+
+    my @nameservers = @{ $resp->{DelegationSet}{NameServers}{NameServer} };
+    print "  nameserver: $_\n" for @nameservers;
+    $self->nameservers(\@nameservers);
+}
 
 no Mouse;
 1;
