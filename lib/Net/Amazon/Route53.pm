3bf6ad427aaac5d573545967d06a41d6df08dcc1
Marco Fontani <mfontani@cpan.org>
2011-01-08 22:52:49 +0000

    Tidied

--- lib/Net/Amazon/Route53.pm
+++ lib/Net/Amazon/Route53.pm
@@ -13,7 +13,7 @@ use Net::Amazon::Route53::HostedZone;
 has 'id'  => ( is => 'rw', isa => 'Str', required => 1, );
 has 'key' => ( is => 'rw', isa => 'Str', required => 1, );
 
-has 'ua'  => (
+has 'ua' => (
     is       => 'rw',
     isa      => 'LWP::UserAgent',
     required => 1,
@@ -30,8 +30,7 @@ sub BUILD {
     my $self = shift;
 }
 
-sub request
-{
+sub request {
     my $self   = shift;
     my $method = shift;
     my $uri    = shift;
@@ -42,7 +41,7 @@ sub request
 
     # Get amazon server's date
     my $date = do {
-        my $rc = $self->ua->get( 'https://route53.amazonaws.com/date' );
+        my $rc = $self->ua->get('https://route53.amazonaws.com/date');
         $rc->header('date');
     };
 
@@ -59,22 +58,20 @@ sub request
     );
 }
 
-sub get_hosted_zones
-{
-    my $self = shift;
-    my $start_marker='';
+sub get_hosted_zones {
+    my $self         = shift;
+    my $start_marker = '';
     my @zones;
-    while (1)
-    {
-        my $rc   = $self->request('get','https://route53.amazonaws.com/2010-10-01/hostedzone?maxitems=100' . $start_marker);
-        my $resp = XML::Bare::xmlin($rc->decoded_content);
+    while (1) {
+        my $rc =
+          $self->request( 'get', 'https://route53.amazonaws.com/2010-10-01/hostedzone?maxitems=100' . $start_marker );
+        my $resp = XML::Bare::xmlin( $rc->decoded_content );
         push @zones, ( ref $resp->{HostedZones} eq 'ARRAY' ? @{ $resp->{HostedZones} } : $resp->{HostedZones} );
         last if $resp->{IsTruncated} eq 'false';
         $start_marker = '?marker=' . $resp->{NextMarker};
     }
     my @o_zones;
-    for my $zone ( @zones )
-    {
+    for my $zone (@zones) {
         push @o_zones,
           Net::Amazon::Route53::HostedZone->new(
             route53 => $self,
