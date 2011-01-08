e1936162339f2ca31d0fe96800afdc52ab9300f1
Marco Fontani <mfontani@cpan.org>
2011-01-08 22:49:27 +0000

    A resource record set

---
+++ lib/Net/Amazon/Route53/ResourceRecordSet.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+package Net::Amazon::Route53::ResourceRecordSet;
+use Mouse;
+use XML::Bare;
+
+has 'hostedzone' => ( is => 'rw', isa => 'Net::Amazon::Route53::HostedZone', required => 1 );
+
+has 'name'   => ( is => 'rw', isa => 'Str',      required => 1 );
+has 'ttl'    => ( is => 'rw', isa => 'Int',      required => 1 );
+has 'type'   => ( is => 'rw', isa => 'Str',      required => 1 );
+has 'values' => ( is => 'rw', isa => 'ArrayRef', required => 1, default => sub { [] } );
+
+no Mouse;
+1;
