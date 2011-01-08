8d043d8d24b7c4089044eb1f763baf8b99c8558f
Marco Fontani <mfontani@cpan.org>
2011-01-08 22:52:05 +0000

    Tidied

--- lib/Net/Amazon/Route53/ResourceRecordSet.pm
+++ lib/Net/Amazon/Route53/ResourceRecordSet.pm
@@ -5,7 +5,7 @@ package Net::Amazon::Route53::ResourceRecordSet;
 use Mouse;
 use XML::Bare;
 
-has 'route53' => ( is => 'rw', isa => 'Net::Amazon::Route53', required => 1, );
+has 'route53'    => ( is => 'rw', isa => 'Net::Amazon::Route53',             required => 1, );
 has 'hostedzone' => ( is => 'rw', isa => 'Net::Amazon::Route53::HostedZone', required => 1 );
 
 has 'name'   => ( is => 'rw', isa => 'Str',      required => 1 );
