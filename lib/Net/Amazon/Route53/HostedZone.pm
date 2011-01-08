df098199c5c5a5d153f41309621c8548edce0ada
Marco Fontani <mfontani@cpan.org>
2011-01-08 22:51:47 +0000

    Tidied

--- lib/Net/Amazon/Route53/HostedZone.pm
+++ lib/Net/Amazon/Route53/HostedZone.pm
@@ -18,38 +18,37 @@ has 'nameservers' => ( is => 'rw', isa => 'ArrayRef[Str]', required => 1, defaul
 
 has 'resource_record_sets' => ( is => 'rw', isa => 'ArrayRef', required => 1, default => sub { [] } );
 
-sub BUILD
-{
+sub BUILD {
     my $self = shift;
 
-    my $rc = $self->route53->request('get','https://route53.amazonaws.com/2010-10-01/' . $self->id);
-    my $resp = XML::Bare::xmlin($rc->decoded_content);
+    my $rc = $self->route53->request( 'get', 'https://route53.amazonaws.com/2010-10-01/' . $self->id );
+    my $resp = XML::Bare::xmlin( $rc->decoded_content );
 
     my @nameservers = @{ $resp->{DelegationSet}{NameServers}{NameServer} };
     print "  nameserver: $_\n" for @nameservers;
-    $self->nameservers(\@nameservers);
+    $self->nameservers( \@nameservers );
 
     my @resource_record_sets;
-    $rc = $self->route53->request('get','https://route53.amazonaws.com/2010-10-01/' . $self->id . '/rrset');
-    $resp = XML::Bare::xmlin($rc->decoded_content);
-    for my $res ( @{ $resp->{ResourceRecordSets}{ResourceRecordSet} } )
-    {
-        push @resource_record_sets, Net::Amazon::Route53::ResourceRecordSet->new(
-            route53 => $self->route53,
+    $rc = $self->route53->request( 'get', 'https://route53.amazonaws.com/2010-10-01/' . $self->id . '/rrset' );
+    $resp = XML::Bare::xmlin( $rc->decoded_content );
+    for my $res ( @{ $resp->{ResourceRecordSets}{ResourceRecordSet} } ) {
+        push @resource_record_sets,
+          Net::Amazon::Route53::ResourceRecordSet->new(
+            route53    => $self->route53,
             hostedzone => $self,
-            name => $res->{Name},
-            ttl => $res->{TTL},
-            type => $res->{Type},
-            values => [
+            name       => $res->{Name},
+            ttl        => $res->{TTL},
+            type       => $res->{Type},
+            values     => [
                 map { $_->{Value} } @{
                     ref $res->{ResourceRecords}{ResourceRecord} eq 'ARRAY'
                     ? $res->{ResourceRecords}{ResourceRecord}
                     : [ $res->{ResourceRecords}{ResourceRecord} ]
                   }
-              ],
-        );
+            ],
+          );
     }
-    $self->resource_record_sets(\@resource_record_sets);
+    $self->resource_record_sets( \@resource_record_sets );
     $self;
 }
 
