2330805ce7f839088d36cc24e9d0a86c8a76f85a
Marco Fontani <mfontani@cpan.org>
2011-01-09 17:21:29 +0000

    Copyrights

--- lib/Net/Amazon/Route53/HostedZone.pm
+++ lib/Net/Amazon/Route53/HostedZone.pm
@@ -194,4 +194,18 @@ sub delete
 }
 
 no Mouse;
+
+=head1 AUTHOR
+
+Marco FONTANI <mfontani@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Marco FONTANI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 1;
